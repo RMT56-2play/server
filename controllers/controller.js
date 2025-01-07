11f22e1fe674cbdd93df1877ca6579f5b24882a6
axxe40 <valih.frenato@gmail.com>
2025-01-07 23:40:54 +0700

    feature: POST /joingame

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,6 +10,37 @@ class Controller {
       console.log(error);
     }
   }
+
+  static async joinGame(req, res) {
+    const { username, gameId } = req.body;
+
+    try {
+      const gameExist = await UserGame.findOne({ where: { gameId } });
+
+      if (!gameExist) {
+        return res.status(404).json({
+          message: "Game not found",
+        });
+      } else {
+        const user = await User.create({ username });
+
+        await UserGame.create({
+          UserId: user.id,
+          GameId: gameId,
+          playerCards: [],
+        });
+
+        return res.status(201).json({
+          message: "User successfully joined the game",
+        });
+      }
+    } catch (error) {
+      console.error("Error joining game:", error);
+      return res.status(500).json({
+        message: "An error occurred while trying to join the game",
+      });
+    }
+  }
 }
 
 module.exports = { Controller };
--- routers/index.js
+++ routers/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const { Controller } = require("../controllers/controller");
 
 router.get("/testing", Controller.getGame);
+router.post("/joingame", Controller.getGame);
 
 module.exports = router;
