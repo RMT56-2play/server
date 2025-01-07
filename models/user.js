421bd70de1ac9555c9f5d213e4e290cc4c581020
BoiRichee <khemalalharits@gmail.com>
2025-01-07 22:27:15 +0700

    association setup

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasOne(models.UserGame, {foreignKey: "UserId"})
     }
   }
   User.init(
--- models/usergame.js
+++ models/usergame.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      UserGame.belongsTo(models.User, {foreignKey: "UserId"})
+      UserGame.hasMany(models.Game, {foreignKey: "GameId"})
     }
   }
   UserGame.init(
