5eb789a735f93c948df813c9210a9e40e3eb1776
axxe40 <valih.frenato@gmail.com>
2025-01-07 23:04:01 +0700

    Add relations between tables in db

--- models/game.js
+++ models/game.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Game.hasMany(models.UserGame, { foreignKey: "GameId" });
     }
   }
   Game.init(
--- models/usergame.js
+++ models/usergame.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      UserGame.belongsTo(models.User, { foreignKey: "UserId" });
+      UserGame.belongsTo(models.Game, { foreignKey: "GameId" });
     }
   }
   UserGame.init(
