1292ea0fea7f91832aae81b9e2d35515dd1ab8c2
Joshuajulio7798 <joshua_julio@ymail.com>
2025-01-08 20:22:47 +0700

    Feat: GET waitplayer

--- routers/index.js
+++ routers/index.js
@@ -5,6 +5,8 @@ router.get("/testing", Controller.getGame);
 router.post("/creategame", Controller.createGame);
 router.post("/joingame", Controller.joinGame);
 
+router.get("/waitplayer", Controller.waitPlayer);
+
 router.get("/startgame", Controller.startGame);
 router.post("/action", Controller.action);
 // router.get("/endgame", Controller.endGame);
